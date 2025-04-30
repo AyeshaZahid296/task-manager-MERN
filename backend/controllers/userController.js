6d3a40d43188a233d47318dd5b17a2f87a12e429
ayeshazahid296 <ayeshazahid296@gmail.com>
2025-04-30 16:59:37 +0500

    grtUsersById api done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,78 +7,45 @@ const bcrypt = require("bcryptjs");
 //@access   Private (Admin)
 const getUsers = async (req, res) => {
     try {
-        const user = await User.find({ role: "member" }).select("password");
+        const users = await User.find({ role: "member" }).select("-password");
 
-        //Add taks coun to each user
         const usersWithTaskCounts = await Promise.all(
             users.map(async (user) => {
-                const pendingTasks = await Tasks.countDocuments({
+                const pendingTasks = await Task.countDocuments({
                     assignedTo: user._id,
                     status: "Pending",
                 });
-                const inProgressTasks = await Tasks.countDocuments({
+                const inProgressTasks = await Task.countDocuments({
                     assignedTo: user._id,
                     status: "In Progress",
                 });
-                const completedTasks = await Tasks.countDocuments({
+                const completedTasks = await Task.countDocuments({
                     assignedTo: user._id,
                     status: "Completed",
                 });
 
                 return {
-                    ...user._doc, // Include all existing user data
+                    ...user._doc,
                     pendingTasks,
                     inProgressTasks,
                     completedTasks,
-
                 };
             })
         );
 
-
+        res.status(200).json(usersWithTaskCounts);
     } catch (error) {
-        res.status(500).json({ message: "Server error", error: error.message })
+        res.status(500).json({ message: "Server error", error: error.message });
     }
-}
+};
+
 
 //@desc     Get a specific User by ID
 //@route    GET /api/users/:id
 //@access   Private 
 const getUserById = async (req, res) => {
     try {
-        const { email, password } = req.body;
-
-        const user = await User.findOne({ email });
-        if (!user) {
-            return res.status(401).json({ message: "Invalid email or password" })
-        }
-
-        //Compare password
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-            return res.status(401).json({ message: "Invalid email or password" })
-        }
-
-        // Return user data with JWT
-        res.json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            role: user.role,
-            profileImageUrl: user.profileImageUrl,
-            token: generateToken(user._id),
-        })
-    } catch (error) {
-        res.status(500).json({ message: "Server error", error: error.message })
-    }
-}
-
-//@desc     Delete a User (Admin only)
-//@route    DELETE /api/users/:id
-//@access   Private (Admin)
-const deleteUser = async (req, res) => {
-    try {
-        const user = await User.findById(req.user.id).select("-password");
+        const user = await User.findById(req.params.id).select("-password");
         if (!user) {
             return res.status(404).json({ message: "User not found" })
         }
@@ -88,4 +55,4 @@ const deleteUser = async (req, res) => {
     }
 }
 
-module.exports = { getUsers, getUserById, deleteUser }
+module.exports = { getUsers, getUserById }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const { adminOnly, protect } = require("../middlewares/authMiddleware");
 const upload = require("../middlewares/uploadMiddleware");
-const { getUsers, getUserById, deleteUser } = require("../controllers/userController");
+const { getUsers, getUserById } = require("../controllers/userController");
 
 const router = express.Router();
 
 //User Management Routes
 router.get("/", protect, adminOnly, getUsers);   //Get all Users (Admin only)
 router.get("/:id", protect, getUserById);   //Get a specific User
-router.delete("/:id", protect, adminOnly, deleteUser);  //Delete a User (Admin only)
 
 module.exports = router
