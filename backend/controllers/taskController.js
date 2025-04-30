66cef853f8ca868dca4d609d794c5e481f89e31c
ayeshazahid296 <ayeshazahid296@gmail.com>
2025-04-30 21:32:46 +0500

    updated updatetask api

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -157,16 +157,34 @@ const createTask = async (req, res) => {
 
 };
 
-//@desc     Get all Users (Admin only)
-//@route    GET /api/users
-//@access   Private (Admin)
+//@desc     Update Task Details
+//@route    PUT /api/tasks
+//@access   Private
 const updateTask = async (req, res) => {
     try {
+        const task = await Task.findById(req.params.id);
+        if (!task) return res.status(404).json({ message: "Task not found" });
 
+        task.title = req.body.title || task.title;
+        task.description = req.body.description || task.description;
+        task.priority = req.body.priority || task.priority;
+        task.dueDate = req.body.dueDate || task.dueDate;
+        task.todoChecklist = req.body.todoChecklist || task.todoChecklist;
+        task.attachments = req.body.attachments || task.attachments;
+
+        if (req.body.assignedTo) {
+            if (!Array.isArray(req.body.assignedTo)) {
+                return res
+                    .status(400)
+                    .json({ message: "assignedTo must be an array of IDs" });
+            }
+            task.assignedTo = req.body.assignedTo;
+        }
+        const updatedTask = await task.save();
+        res.json({ message: "Task updated successfully", updateTask });
     } catch (error) {
         res.status(500).json({ message: "Server error", error: error.message });
     }
-
 };
 
 
