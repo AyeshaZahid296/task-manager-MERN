1dc73f61d51efa779239c696b002f7a47fb50070
ayeshazahid296 <ayeshazahid296@gmail.com>
2025-05-09 23:33:39 +0500

    authLayout updated

--- frontend/src/components/layout/AuthLayout.jsx
+++ frontend/src/components/layout/AuthLayout.jsx
@@ -3,13 +3,13 @@ import uiImg from '../../assets/task-management-illu.avif';
 
 const AuthLayout = ({ children }) => {
     return (
-        <div className='flex' {
-        }>
-            <div>
-                <h2></h2>
+        <div className='flex'>
+            <div className='w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12'>
+                <h2 className='text-lg font-medium text-black'>Task Manager</h2>
+                {children}
             </div>
-            <div>
-                <img src="" alt="" />
+            <div className='hidden md:flex w-[40vw] h-screen items-center justify justify-center bg-blue-50 bg-[url(/task-priority-preview.jpg)] bg-cover bg-no-repeat overflow-hidden p-18' >
+                <img src={uiImg} className='w-64 lg-w-[90%]' />
             </div>
         </div>
     )
