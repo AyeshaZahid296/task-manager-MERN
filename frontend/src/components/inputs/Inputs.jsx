f145c1e02bdc33b3627d01c80415bb64ec2897b2
ayeshazahid296 <ayeshazahid296@gmail.com>
2025-05-11 21:35:28 +0500

    updated

---
+++ frontend/src/components/inputs/Inputs.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+
+const Inputs = ({ value, onChange, label, placeholder, type }) => {
+  const [showPassword, setPassword] = useState("");
+  return (
+    <div>Inputs</div>
+  )
+}
+
+export default Inputs
