aa6bfeb71f16361596ba353a421acebf8268045f
ayeshazahid296 <ayeshazahid296@gmail.com>
2025-07-16 23:39:34 +0500

    login done

--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import AuthLayout from '../../components/layout/AuthLayout'
 
 const Register = () => {
+    const [profilePic, setProfilePic] = useState(null);
+    const [fullName, setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [adminInviteTocken, setAdminInviteTocken] = useState("");
+
+    const [error, setError] = useState(null);
+
     return (
-        <div>Register</div>
+        <AuthLayout>
+            <div className='lg:w-[100%] h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center'>
+                <h3 className='text-xl font-semibold text-black'>Create an Account</h3>
+                <p className=''>
+                    Join us today by entering your details below.
+                </p>
+            </div>
+        </AuthLayout>
     )
 }
 
