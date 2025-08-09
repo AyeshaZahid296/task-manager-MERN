c07f83aeafc2a3174760621031a628cb4a8c823c
ayeshazahid296 <ayeshazahid296@gmail.com>
2025-08-09 20:33:55 +0500

    Delete alert added

--- frontend/src/pages/Admin/CreateTask.jsx
+++ frontend/src/pages/Admin/CreateTask.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import DashboardLayout from '../../components/layout/DashboardLayout'
 import { PRIORITY_DATA } from '../../utils/data'
 import axiosInstance from '../../utils/axiosInstance'
@@ -11,6 +11,7 @@ import SelectionDropdown from '../../components/inputs/SelectionDropdown'
 import SelectUsers from '../../components/inputs/SelectUsers'
 import AddAttachmentsInput from '../../components/inputs/AddAttachmentsInput'
 import TodoListInput from '../../components/inputs/TodoListInput'
+import Modal from '../../components/Modal'
 
 const CreateTask = () => {
     const location = useLocation();
@@ -78,7 +79,37 @@ const CreateTask = () => {
     };
 
     // Update Task
-    const updateTask = async () => { };
+    const updateTask = async () => {
+        setLoading(true);
+
+        try {
+            const todolist = taskData.todoChecklist?.map((item) => {
+                const prevTodochecklist = currentTask?.todoChecklist || [];
+                const matchedTask = prevTodochecklist.find((task) => task.text == item);
+
+                return {
+                    text: item,
+                    completed: matchedTask ? matchedTask.completed : false,
+                };
+            });
+
+            const response = await axiosInstance.put(
+                API_PATHS.TASKS.UPDATE_TASKS(taskId),
+                {
+                    ...taskData,
+                    dueDate: new Date(taskData.dueDate).toISOString(),
+                    todoChecklist: todolist,
+                }
+            );
+
+            toast.success("Task Updated Successfully");
+        } catch (error) {
+            console.error("Error creating task:", error);
+            setLoading(false);
+        } finally {
+            setLoading(false);
+        }
+    };
 
     const handleSubmit = async () => {
         setError(null);
@@ -112,10 +143,55 @@ const CreateTask = () => {
     };
 
     // get Task info by ID
-    const getTaskDetailsByID = async () => { };
+    const getTaskDetailsByID = async () => {
+        try {
+            const response = await axiosInstance.get(
+                API_PATHS.TASKS.GET_ALL_TASKS_BY_ID(taskId)
+            );
+
+            if (response.data) {
+                const taskInfo = response.data;
+                setCurrentTask(taskInfo);
+
+                setTaskData((prevState) => ({
+                    title: taskInfo.title,
+                    description: taskInfo.description,
+                    priority: taskInfo.priority,
+                    dueDate: taskInfo.dueDate
+                        ? moment(taskInfo.dueDate).format("YYYY-MM-DD")
+                        : null,
+                    assignedTo: taskInfo?.assignedTo?.map((item) => item?._id) || [],
+                    todoChecklist:
+                        taskInfo?.todoChecklist?.map((item) => item?.text) || [],
+                    attachments: taskInfo?.attachments || [],
+                }));
+            }
+        } catch (error) {
+            console.error("Error fetching users:", error);
+        }
+    };
 
     // Delete Task
-    const deleteTask = async () => { };
+    const deleteTask = async () => {
+        try {
+            await axiosInstance.delete(API_PATHS.TASKS.DELETE_TASKS(taskId));
+
+            setOpenDeleteAlert(false);
+            toast.success("Expense details deleted sucessfully");
+            navigate('/admin/tasks')
+        } catch (error) {
+            console.error(
+                "Error deleting expense:",
+                error.response?.data?.message || error.message
+            );
+        }
+    };
+
+    useEffect(() => {
+        if (taskId)
+            getTaskDetailsByID(taskId);
+        return () => { };
+    }, [taskId])
 
     return (
         <DashboardLayout activeMenu="Create Task">
@@ -254,6 +330,17 @@ const CreateTask = () => {
                     </div>
                 </div>
             </div>
+
+            <Modal
+                isOpen={openDeleteAlert}
+                isClose={() => setOpenDeleteAlert(false)}
+                title="Delete Task"
+            >
+                <DeleteAlert
+                    content="Are yu sure you want to delete this task?"
+                    onDelete={() => deleteTask()}
+                />
+            </Modal>
         </DashboardLayout>
     )
 }
