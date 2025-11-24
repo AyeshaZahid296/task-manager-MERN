6e962d20f4dd67f5c5a721a5680a6a61279d7ba2
AyeshaZahid296 <ayeshazahid296.com>
2025-11-24 22:22:21 +0500

    ./task-priority-preview.jpg error corrected

--- frontend/src/components/layout/AuthLayout.jsx
+++ frontend/src/components/layout/AuthLayout.jsx
@@ -8,7 +8,7 @@ const AuthLayout = ({ children }) => {
                 <h2 className='text-lg font-medium text-black'>Task Manager</h2>
                 {children}
             </div>
-            <div className='hidden md:flex w-[40vw] h-screen items-center justify justify-center bg-blue-50 bg-[url(/task-priority-preview.jpg)] bg-cover bg-no-repeat overflow-hidden p-18' >
+            <div className='hidden md:flex w-[40vw] h-screen items-center justify justify-center bg-blue-50 bg-img bg-cover bg-no-repeat overflow-hidden p-18' >
                 <img src={uiImg} className='w-64 lg-w-[90%]' />
             </div>
         </div>
